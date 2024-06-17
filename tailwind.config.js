02618422d234f8df484eb2df8c22cee2bede961d
Jakib Isherwood <jakibisherwood88@gmail.com>
2024-06-17 23:16:51 +1200

    Good starting point

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.html',
+        './src/**/*.ts',
+        './src/**/*.tsx',
+        './src/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
